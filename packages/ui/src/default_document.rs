28d9762d21108f5de8143430ac5bedbf785b946e
Dave Abrahams <dabrahams@adobe.com>
2025-08-18 11:37:20 -0700

    Add a circle

--- packages/ui/src/default_document.rs
+++ packages/ui/src/default_document.rs
@@ -1,6 +1,7 @@
 use crate::document::*;
 use svg::node::element::Path;
 use svg::node::element::path::Data;
+use svg::node::element::Circle;
 
 pub fn default_document() -> Document {
     let square = Data::new()
@@ -16,7 +17,19 @@ pub fn default_document() -> Document {
         .set("stroke-width", 3)
         .set("d", square);
 
-    Document::new()
+
+    let circle = Circle::new()
+    .set("cx", 35)
+    .set("cy", 35)
+    .set("r", 30)
+    .set("fill", "none")
+    .set("stroke", "black")
+    .set("stroke-width", 3);
+
+    let r = Document::new()
         .set("viewBox", (0, 0, 70, 70))
         .add(path)
+        .add(circle);
+//    svg::save("image.svg", &r).unwrap();
+    r
 }
