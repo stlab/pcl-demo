5665ee16b1cfaccc8655297fb1cb047d9c38c959
Dave Abrahams <dabrahams@adobe.com>
2025-09-08 12:33:05 -0700

    Write out the doc as json.

--- packages/ui/src/document.rs
+++ packages/ui/src/document.rs
@@ -1,7 +1,11 @@
-use std::{fs::File, io::Read, io::Result};
-use std::path::Path;
+use std::fs::*;
+use std::io::*;
+use std::path::*;
+use serde::{Deserialize, Serialize};
+use serde_json::*;
 
 /// In-memory representation of a pcl-demo document.
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Document {
     html: String
 }
@@ -9,9 +13,15 @@ pub struct Document {
 impl Document {
 
     /// Returns the in-memory representation of the pcl-demo document at `p`.
-    pub fn new_from_file<P: AsRef<Path>>(p: P) -> Result<Self> {
-        let mut r = Document { html: String::new() };
-        File::open(p)?.read_to_string(&mut r.html).map(|_| r)
+    pub fn new_from_file<P: AsRef<Path>>(p: P) -> std::io::Result<Self> {
+        let mut f = File::create(p.as_ref().with_file_name("image.json"))?;
+        let r = Document {
+            html: std::fs::read_to_string(p)?
+        };
+        let j = serde_json::to_string_pretty(&r)?;
+        let _ = f.write(j.as_bytes())?;
+
+        Ok(r)
     }
 
     /// Returns the HTML to render `self`.
