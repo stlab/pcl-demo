766ea097127aa3ef5fd2b6da4af883ebf2b3bc07
Dave Abrahams <dabrahams@adobe.com>
2025-10-01 08:28:21 -0700

    Emacs: format rust files on save.

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((rust-mode . ((rust-format-on-save . t)))
+ (rust-ts-mode . ((rust-format-on-save . t)))
+ ("rustic-mode" . ((rust-format-on-save . t))))
